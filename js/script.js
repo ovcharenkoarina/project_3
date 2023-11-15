c789aaf6d3c7ef38d22981b1d6cfdd56a997fe19
ovcharenkoarina <aovcharenko@hawk.iit.edu>
2023-11-14 23:56:44 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
-function init (){
- //alert("it works!")
- const center = { lat: -34.397, lng: 150.644 };
+function init(){
+  var el = document.getElementById('canvas');
+  var myLocation = new google.maps.LatLng(53.186431, 50.095774);
   
-  // Map with the specified center and zoom level
-  const map = new google.maps.Map(document.getElementById("map"), {
+  var mapOptions = {
+    center: myLocation,
     zoom: 8,
-    center: center,
-  });
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    
+	mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+  }
+};
 
-  // Marker
-  const marker = new google.maps.Marker({
-    position: center,
-    map: map,
-    title: 'Marker Title',
-  });
+var myMap = new google.maps.Map(el, mapOptions);
 
-  // Add a zoom control to the map
-  const zoomControl = new google.maps.ZoomControlOptions();
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(zoomControl);
+var marker = new google.maps.Marker({
+  position: myLocation,
+  map: myMap,
+  animation: google.maps.Animation.DROP,
+  icon: 'images/icon.jpg'
+});
 
-  // Custom map styling
-  const style = [
-    {
-      featureType: 'poi',
-      elementType: 'labels',
-      stylers: [{ visibility: 'off' }],
-    },
-    {
-      featureType: 'transit',
-      elementType: 'labels',
-      stylers: [{ visibility: 'off' }],
-    },
-  ];
-  map.setOptions({ styles: style });
+var contentString = '<h1>Leningradskaya Street</h1><p>Leningradskaya Street is one of the best places to spend time in Samara, Russia</p>';
+
+var infowindow = new google.maps.InfoWindow ({
+  content: contentString
+});
+
+google.maps.event.addListener(marker, 'mouseover', function() {
+  infowindow.open(myMap, marker);
+});
+
 }
-google.maps.event.addListener(window, "load", init);
+
+
+google.maps.event.addDomListener(window, 'load', init);
